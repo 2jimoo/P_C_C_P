9b0ec3c1ef2f1a4bdc2489f4985b31e8d659fd92
huijeong.son <huijeong.son@linecorp.com>
2023-03-12 15:44:36 +0900

    1,2,3 떨어뜨리기

---
+++ retry/0312_123_떨어뜨리기.py
@@ -0,0 +1,3 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/150364/solution_groups?language=python3&page=2
+# https://school.programmers.co.kr/questions/43576
+# https://tech.kakao.com/2023/01/25/2023-kakao-recruitment-round-1/
